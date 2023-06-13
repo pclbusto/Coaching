92a8b56a072e96b3603ccc7cd7ffaf53e28aa47b
Pedro Cesar Luis Busto <pclbusto@gmail.com>
2023-06-13 14:55:19 -0300

    1111

--- coaching_sessions/admin.py
+++ coaching_sessions/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Recurso,Info_Sesion
 
-admin.site.register(Recurso)
-admin.site.register(Info_Sesion)
+#admin.site.register(Recurso)
+
+
 
 # Register your models here.
+class RecursoAdmin(admin.ModelAdmin):
+    list_display = ["dni", "apellido","nombre"]
+    fields = ["apellido","nombre", "dni"]
+class Info_SesionAdmin(admin.ModelAdmin):
+    list_display = ["fecha", "hora","descripcion", "recurso"]
+    list_filter = ["recurso"]
+
+admin.site.register(Recurso, RecursoAdmin)
+admin.site.register(Info_Sesion, Info_SesionAdmin)
--- coaching_sessions/models.py
+++ coaching_sessions/models.py
@@ -8,9 +8,13 @@ class Recurso(models.Model):
     nombre = models.CharField(max_length=200)
     apellido = models.CharField(max_length=100)
     dni = models.CharField(max_length=8)
+    def __str__(self):
+        return ("{} {}".format(self.apellido, self.nombre))
 
 class Info_Sesion(models.Model):
     fecha = models.DateField("Fecha Creación", default=date.today())
     hora = models.TimeField("Hora Creación", default=timezone.now)
     descripcion = models.TextField(max_length=8000)
     recurso = models.ForeignKey(Recurso, on_delete=models.DO_NOTHING, null=True)
+
+
